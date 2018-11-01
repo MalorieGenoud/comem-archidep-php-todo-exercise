48d2fd77aa0b70afbf4a7fb03154b7eb0f3e8dc6
MalorieGenoud <malorie.genoud@gmail.com>
2018-11-01 12:46:37 +0100

    Add function toggle

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ switch($_POST['action']) {
 
     $id = $_POST['id'];
     if(is_numeric($id)) {
-      $updateQuery = ''; // IMPLEMENT ME
+      $updateQuery = 'UPDATE todo SET done = 0 WHERE id '.$id;
       if(!$db->query($updateQuery)) {
         die(print_r($db->errorInfo(), true));
       }
